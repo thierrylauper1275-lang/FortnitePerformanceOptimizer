bd3573e5281edf4dd0ac179ea69dde494170fc40
thierrylauper1275-lang <thierrylauper1275@gmail.com>
2026-09-15 14:58:38 +0200

    Step 7: Add remaining Views (System, Benchmark)

---
+++ src/Views/BenchmarkView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace FortnitePerformanceOptimizer.Views;
+
+public partial class BenchmarkView : UserControl
+{
+    public BenchmarkView()
+    {
+        InitializeComponent();
+    }
+}
